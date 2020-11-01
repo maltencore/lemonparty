de45f5ba902304498fd2b7712b27881ec86537c5
maltencore <73798010+maltencore@users.noreply.github.com>
2020-11-01 23:17:06 +0100

    prova

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,7 @@
                   </v-tab>
 
                   <v-tab href="#tabpotenziamenti">
-                    Potenziamenti
+                    Potenziamentissssssss
                     <v-icon>mdi-trending-up</v-icon>
                   </v-tab>
 
